c4b5018f636275af3283fe461feb4a84360d5ac5
Sagarikac310 <sagarikac310@gmail.com>
2020-03-23 01:26:18 +0530

    Timer and notification added

---
+++ elements-master(copy)/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/sagarika/Desktop/pedromassango-xtimer-flutter-app-f6a3d89"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/opt/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=3.6.0"
+export "FLUTTER_BUILD_NUMBER=6"
